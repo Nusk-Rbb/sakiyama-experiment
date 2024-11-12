15ee66f5e4399e826628c10cc372b51e7ddff688
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-11-13 01:56:45 +0900

    fix: create sample table

--- kadai4/app/db/sample.sql
+++ kadai4/app/db/sample.sql
@@ -4,7 +4,7 @@ CREATE TABLE sample_tbl (
     id   INT  PRIMARY KEY,
     name  TEXT,
     score INT
-)
+);
 
 INSERT INTO sample_tbl (id, name, score) VALUES ( '100', '青木', '90' );
 INSERT INTO sample_tbl (id, name, score) VALUES ( '110', '池田', '85' );
