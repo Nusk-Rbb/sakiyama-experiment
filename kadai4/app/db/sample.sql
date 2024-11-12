367b0b17f7e09c1e6733b597eace6aca5dd06da6
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-11-13 01:57:40 +0900

    update: id to uid

--- kadai4/app/db/sample.sql
+++ kadai4/app/db/sample.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS sample_tbl;
 
 CREATE TABLE sample_tbl (
-    id   INT  PRIMARY KEY,
+    uid   INT  PRIMARY KEY,
     name  TEXT,
     score INT
 );
 
-INSERT INTO sample_tbl (id, name, score) VALUES ( '100', '青木', '90' );
-INSERT INTO sample_tbl (id, name, score) VALUES ( '110', '池田', '85' );
-INSERT INTO sample_tbl (id, name, score) VALUES ( '120', '上田', '90' );
-INSERT INTO sample_tbl (id, name, score) VALUES ( '130', '佐藤', '95' );
-INSERT INTO sample_tbl (id, name, score) VALUES ( '140', '田中', '80' );
+INSERT INTO sample_tbl (uid, name, score) VALUES ( '100', '青木', '90' );
+INSERT INTO sample_tbl (uid, name, score) VALUES ( '110', '池田', '85' );
+INSERT INTO sample_tbl (uid, name, score) VALUES ( '120', '上田', '90' );
+INSERT INTO sample_tbl (uid, name, score) VALUES ( '130', '佐藤', '95' );
+INSERT INTO sample_tbl (uid, name, score) VALUES ( '140', '田中', '80' );
 
 GRANT ALL ON sample_tbl TO apache;
