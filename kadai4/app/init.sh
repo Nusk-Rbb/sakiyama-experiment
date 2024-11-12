96ab93337576ebec0b6c49d4893287b6c7499e8f
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-11-13 01:55:35 +0900

    update: add sampel table

--- kadai4/app/init.sh
+++ kadai4/app/init.sh
@@ -1,3 +1,4 @@
 docker cp db postgres:/tmp/
 docker exec -it postgres psql -U apache -d www -f /tmp/db/init.sql
 docker exec -it postgres psql -U apache -d www -f /tmp/db/seiseki.sql
+docker exec -it postgres psql -U apache -d www -f /tmp/db/sample.sql
