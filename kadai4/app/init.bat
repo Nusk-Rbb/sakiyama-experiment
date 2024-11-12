0c093b101657408bded65d765b20db93ca4ccf89
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-11-12 18:36:27 +0900

    fix: seiseki.php

--- kadai4/app/init.bat
+++ kadai4/app/init.bat
@@ -1,1 +1,2 @@
 psql -h localhost -p 5432 -U apache -d www -f db/init.sjis.sql
+psql -h localhost -p 5432 -U apache -d www -f samples/pgsql/seiseki.sjis.sql
--- kadai4/app/init.sh
+++ kadai4/app/init.sh
@@ -1,1 +1,2 @@
 psql -h localhost -p 5432 -U apache -d www -f db/init.utf8.sql
+psql -h localhost -p 5432 -U apache -d www -f samples/pgsql/seiseki.utf8.sql
